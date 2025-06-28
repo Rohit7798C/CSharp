eb9df1e7d207591f8eba41ddb0b406a703db32eb
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-06-28 22:41:14 +0530

    Add files via upload

---
+++ 2_sum.cs
@@ -0,0 +1,8 @@
+// int a,b,c;
+// Console.Write("Enter a : ");
+// a=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter b : ");
+// b=Convert.ToInt32(Console.ReadLine());
+// c=a+b;
+// Console.WriteLine("The sum of two numbers is : "+c);
+        
