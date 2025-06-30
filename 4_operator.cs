5fe64da6f99d1dd2a3d5a519fe2b46713438ea09
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-06-30 23:03:06 +0530

    Add files via upload

---
+++ 4_operator.cs
@@ -0,0 +1,53 @@
+// Operators
+/*
+- Operator is a symbol that operates on a value or variable.
+- It is used to perform logical & mathematical operations.
+
+Types of Operators
+1) Arithmetic (+,-,*,/,%)
+2) Relational (<,>,<=,>=)
+3) Equality (==,!=,===)
+4) Logical (!,&&,||,&,|)
+5) Assignment (=)
+6) Increment & Decrement (++,--)
+7) Ternary Operator (? :)
+*/
+
+// int a=100,b=50;
+
+// 1) Arithmetic (+,-,*,/,%)
+// Console.WriteLine(a+b);
+// Console.WriteLine(a-b);
+// Console.WriteLine(a*b);
+// Console.WriteLine(a/b);
+// Console.WriteLine(a%b);
+
+// 2) Relational (<,>,<=,>=)
+// Console.WriteLine(a<b);
+// Console.WriteLine(a>b);
+// Console.WriteLine(a<=b);
+// Console.WriteLine(a>=b);
+
+// 3) Equality (==,!=)
+// Console.WriteLine(a==b);
+// Console.WriteLine(a!=b);
+
+// 4) Logical (!,&&,||,&,|)
+// Console.WriteLine(a<b && a>b);
+// Console.WriteLine(a<b || a>b);
+// Console.WriteLine(!(a<b));
+
+// 5) Ternary Operator (? :)
+
+// int r = (a>b)? a:b;
+// Console.WriteLine(r);
+
+// 6) Increment & Decrement (++,--)
+// Console.WriteLine(++a);     //101 
+// Console.WriteLine(a++);     //101 --> 102
+// Console.WriteLine(a--);     //102 -->101
+// Console.WriteLine(--a);     //100
+
+
+
+
