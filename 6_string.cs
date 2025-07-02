ab09cb05b52dd93b11ccdf128b7357a766026086
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-07-02 23:31:56 +0530

    Add files via upload

---
+++ 6_string.cs
@@ -0,0 +1,35 @@
+/* String 
+- String is nothing but Sequence of many characters which are enclosed by double
+  quotes.
+- Ex: "ANKUSH" is a string, it is madeup a sequence of 6-characters.
+
+Q. Print String.
+Q. Find string Length.
+Q. Concate two strings.
+Q. Convert string upper to lower & vice-versa
+Q. Reverse String
+Q. Compare two strings.
+
+*/
+
+/*
+//WriteLine() doesn't work for strings
+//We can use Write() only.
+
+string str="Akhilesh",str2="Kumar";
+int length = str.Length;
+Console.WriteLine("Length of str : " + length);
+Console.Write(str[2]);             //Printing element at index-2 of str.
+Console.WriteLine();
+Console.Write(str.Equals(str2));   //Checking Whether strings are equal.
+Console.WriteLine();
+Console.Write(str+" "+str2);       //Concatenate the two strings.
+Console.WriteLine();
+Console.Write(str.ToUpper());      // Converting to Upper-Case.
+Console.WriteLine();
+Console.Write(str.ToLower());      // Converting to Lower-Case.
+Console.WriteLine();
+for(int i=7;i>=0;i--){
+    Console.Write(str[i]);         // Reversing the String.
+}
+*/
