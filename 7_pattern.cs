40d7f332d045216aec799c9c31ddef228981eb02
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-07-03 13:10:14 +0530

    Add files via upload

---
+++ 7_pattern.cs
@@ -0,0 +1,22 @@
+// for(int i=1;i<=3;i++){         //rows
+//     for(int j=1;j<=i;j++){     //columns
+//         Console.Write("* ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// for(int i=1;i<=5;i++){         //rows
+//     for(int j=5;j>=i;j--){     //columns
+//         Console.Write("* ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// for(int i=1;i<=5;i++){         //rows
+//     for(int j=1;j<=5;j++){     //columns
+//         Console.Write("* ");
+//     }
+//     Console.WriteLine();
+// }
