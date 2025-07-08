480f9f5b099d0117180e9d09759049b344d47b16
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-07-08 12:23:20 +0530

    Add files via upload

---
+++ 12_polymorphism.cs
@@ -0,0 +1,10 @@
+/* Polymorphism 
+- Polymorphism means "many-form", in other words, one name with multiple functionalities
+
+- Types:
+1) Compile-time: We perform function-overloading.
+2) Runtime : We perform function-overriding(inheritance).
+
+
+
+*/
